475e7df8ee2436d483bdbe3346d489aa77845144
funzeye <funzeye@gmail.com>
2015-03-27 15:56:21 +0000

    v1.2

--- Phaser Project Template - Javascript/app.js
+++ Phaser Project Template - Javascript/app.js
@@ -1,20 +1,15 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-var SimpleGame = (function () {
-    function SimpleGame() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
-    }
-    SimpleGame.prototype.preload = function () {
-        this.game.load.image('logo', 'img/phaser2.png');
-    };
-    SimpleGame.prototype.create = function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-        logo.scale.setTo(0.2, 0.2);
-        this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
-    };
-    return SimpleGame;
-})();
+function preload() {
+    game.load.image('logo', 'assets/phaser2.png');
+}
 
-window.onload = function () {
-    var game = new SimpleGame();
-};
+function create() {
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
+    logo.scale.setTo(0.2, 0.2);
+    game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
+}
+
+function update() {
+}
